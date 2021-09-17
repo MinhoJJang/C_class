99982df64af3e3da9cfc94b24ed2dd707ec44a2d
Minho Jang <84257033+MinhoJJang@users.noreply.github.com>
2021-09-17 12:35:26 +0900

    Add files via upload

---
+++ 01_HelloWorld/HelloWorld.c
@@ -0,0 +1,25 @@
+#include <stdio.h>	
+
+int main ()	{
+   /* int main()의 int는 main함수가 return을 사용해 
+	반환하는 값이 '정수'라는 의미이다. */
+
+	printf("helloworld\n");
+	//return 0;
+	printf("helloworld\n");
+
+getchar();
+return 0; 
+//운영체제 Windows에게 함수가 정상적으로 종료되었다는 의미를 전달하기 위함 
+//또한 컴파일러가 이 코드를 읽는 순간 읽기를 멈춘다. 
+//0을 반환하면 성공, 1을 반환하면 실패를 의미
+//생략가능 
+
+}
+
+// 블럭 { .. } 
+// 괄호 ( .. ) 
+// 따옴표 " .. " 
+//   이들의 쌍이 맞아야 한다
+
+
---
+++ 01_HelloWorld/myworld.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+	printf("Hi\n");
+	printf("c언어 월수반입니다\n");
+	printf("ENTER을 누르면 프로그램이 종료됩니다\n");
+
+	getchar();
+
+	return 0;
+
+}
